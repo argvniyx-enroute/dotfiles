b92c7c2f8230b7c7ed883bc57d29e13863914297
Ricardo Garza <ricardo.garza@betterment.com>
2022-02-07 14:55:05 -0600

    Add Hammerspoon and Amethyst dotfiles

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,12 @@
+hs.loadSpoon("SpoonInstall")
+
+spoon.SpoonInstall:andUse("AppLauncher", {
+  hotkeys = {
+    s = "Google Chrome",
+    e = "Emacs",
+    l = "Slack",
+    t = "Kitty",
+    p = "Spotify",
+    z = "Zoom.us"
+  }
+})
